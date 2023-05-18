f59bcd9f0fd621020c2ed089eabe9d8b00ad4982
MinecraftMikey20 <95052667+MinecraftMikey20@users.noreply.github.com>
2023-05-18 23:26:06 +0200

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Kirby standing still":return img`
+. . . . . . f f f f f . . . . . 
+. . . . f f b 3 3 3 b f f . . . 
+. . . f b 3 3 3 3 3 3 3 b f . . 
+. . f b 3 3 3 3 3 3 3 3 3 f . . 
+. . f 3 3 3 3 3 3 3 3 3 3 b f . 
+. f 3 3 3 3 3 3 3 f 3 f 3 b f . 
+f b 3 3 3 3 3 3 3 f 3 f 3 3 3 f 
+f 3 3 3 3 3 3 3 3 f 3 f 3 3 3 f 
+f 3 3 3 3 3 3 3 3 3 3 3 b b 3 f 
+f b 3 3 b 3 3 3 3 3 3 3 3 b 3 f 
+. f b 3 f 3 3 3 3 3 f 3 3 f b f 
+. . f f b 3 3 3 3 3 3 3 b f f . 
+. . . f f b b 3 3 3 3 b f f . . 
+. . f b b f f f f f f f b b f . 
+. f b b b b b f f f f b b b b f 
+. . f f f f f . . . f f f f f . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,26 @@
+scene.setBackgroundColor(7)
+let mySprite =  sprites.create(assets.image`Kirby standing still`, SpriteKind.Player)
+controller.moveSprite(mySprite)
+let pizza = sprites.create(img`
+    . . . . . . . e c 7 . . . . . .
+    . . . . e e e c 7 7 e e . . . .
+    . . c e e e e c 7 e 2 2 e e . .
+    . c e e e e e c 6 e e 2 2 2 e .
+    . c e e e 2 e c c 2 4 5 4 2 e .
+    c e e e 2 2 2 2 2 2 4 5 5 2 2 e
+    c e e 2 2 2 2 2 2 2 2 4 4 2 2 e
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e
+    c e e 2 2 2 2 2 2 2 2 2 2 2 2 e
+    c e e 2 2 2 2 2 2 2 2 2 2 4 2 e
+    . e e e 2 2 2 2 2 2 2 2 2 4 e .
+    . 2 e e 2 2 2 2 2 2 2 2 4 2 e .
+    . . 2 e e 2 2 2 2 2 4 4 2 e . .
+    . . . 2 2 e e 4 4 4 2 e e . . .
+    . . . . . 2 2 e e e e . . . . .
+`, SpriteKind.Food)
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    otherSprite.setPosition(randint(0,160), randint(0,120))
+    info.startCountdown(10)
+})
